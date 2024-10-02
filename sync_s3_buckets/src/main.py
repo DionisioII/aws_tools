9901707801c7b99b4f037f6d605e3caa880214ef
giorgio aurispa <jaws24@hotmail.it>
2024-10-02 14:44:32 +0200

    update command

--- sync_s3_buckets/src/main.py
+++ sync_s3_buckets/src/main.py
@@ -11,8 +11,9 @@ def main():
     src_bucket = os.environ.get('SOURCE_BUCKET')
     dst_bucket = os.environ.get('DESTINATION_BUCKET')
     additional_line_params = os.environ.get('ADDITIONAL_LINE_PARAMS', '') #for example ' --exclude "*" --include "folder/*" '
-    command = f"sync s3://{src_bucket}/ s3://{dst_bucket}/ {additional_line_params}"
+    command = f"s3 sync s3://{src_bucket}/ s3://{dst_bucket}/ {additional_line_params}"
     aws_cli_command = f"aws {command} "
+    print(aws_cli_command)
     result = subprocess.run(aws_cli_command, shell=True, capture_output=True, text=True)
     print(result.stdout)
     print(result.stderr)
