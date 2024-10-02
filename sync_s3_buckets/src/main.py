60c41fef98369c711f53bb810c75431272fdcf1a
giorgio aurispa <jaws24@hotmail.it>
2024-10-02 12:15:13 +0200

    DockerFile

--- sync_s3_buckets/src/main.py
+++ sync_s3_buckets/src/main.py
@@ -5,12 +5,19 @@ import boto3
 
 def main():
 
+    print("main called")
     src_bucket = os.environ.get('SOURCE_BUCKET')
     dst_bucket = os.environ.get('DESTINATION_BUCKET')
     additional_line_params = os.environ.get('ADDITIONAL_LINE_PARAMS', '') #for example ' --exclude "*" --include "folder/*" '
     command = f"sync s3://{src_bucket}/ s3://{dst_bucket}/ {additional_line_params}"
     aws_cli_command = f"aws {command} "
     result = subprocess.run(aws_cli_command, shell=True, capture_output=True, text=True)
+
+    print("syncing terminated")
     
     return result
+
+
+if __name__ == '__main__':
+    main()
 
